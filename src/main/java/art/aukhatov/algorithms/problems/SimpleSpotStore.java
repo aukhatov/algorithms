7943ccf4730b44253a86507a0cbc480c11dc3180
Artur Aukhatov <aaukhatov@gmail.com>
2020-10-27 22:40:18 +0300

    Spot Store: Get nearest spot value, using BinaryTree

--- src/main/java/art/aukhatov/algorithms/problems/SimpleSpotStore.java
+++ src/main/java/art/aukhatov/algorithms/problems/SimpleSpotStore.java
@@ -2,27 +2,33 @@ package art.aukhatov.algorithms.problems;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.NavigableMap;
 import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class SimpleSpotStore implements SpotStore {
 
-    private final Map<String, Map<LocalDateTime, Double>> storage = new ConcurrentHashMap<>();
+    private final Map<String, NavigableMap<LocalDateTime, Double>> storage = new ConcurrentHashMap<>();
 
     @Override
     public void add(String ccypair, double spot, LocalDateTime tickTime) {
-        storage.computeIfAbsent(ccypair, f -> new ConcurrentHashMap<>());
-        Map<LocalDateTime, Double> spotStore = storage.get(ccypair);
+        storage.computeIfAbsent(ccypair, f -> new TreeMap<>(LocalDateTime::compareTo));
+        NavigableMap<LocalDateTime, Double> spotStore = storage.get(ccypair);
         spotStore.put(tickTime, Double.valueOf(spot));
     }
 
     @Override
     public double get(String ccypair, LocalDateTime dateTime) {
-        Map<LocalDateTime, Double> spotStore = storage.get(ccypair);
+        NavigableMap<LocalDateTime, Double> spotStore = storage.get(ccypair);
         if (Objects.nonNull(spotStore)) {
-            // there has to implement Binary Tree to get latest tick time spot value
-            return spotStore.get(dateTime);
+            Map.Entry<LocalDateTime, Double> entry = spotStore.floorEntry(dateTime);
+            if (Objects.nonNull(entry)) {
+                return entry.getValue();
+            } else {
+                throw new NoSuchElementException("Spot value not found");
+            }
         }
         throw new NoSuchElementException("Spot value not found");
     }
--- src/test/java/art/aukhatov/algorithms/problems/SimpleSpotStoreTest.java
+++ src/test/java/art/aukhatov/algorithms/problems/SimpleSpotStoreTest.java
@@ -17,7 +17,18 @@ public class SimpleSpotStoreTest {
         store.add("USDRUB", 70, LocalDateTime.of(2020, 10, 2, 12, 15, 0, 0));
         store.add("USDRUB", 56, LocalDateTime.of(2020, 10, 5, 12, 15, 0, 0));
 
-
         assertEquals(expectedSpotValue, store.get("USDRUB", tickTime), 0.0);
+    }
+
+    @Test
+    public void shouldReturnNearestSpot() {
+        SimpleSpotStore store = new SimpleSpotStore();
+        LocalDateTime tickTime = LocalDateTime.of(2020, 10, 1, 12, 15, 0, 0);
+        double expectedSpotValue = 76.45;
+        store.add("USDRUB", expectedSpotValue, tickTime);
+        store.add("USDRUB", 70, LocalDateTime.of(2020, 10, 2, 12, 15, 0, 0));
+        store.add("USDRUB", 56, LocalDateTime.of(2020, 10, 5, 12, 15, 0, 0));
+
+        assertEquals(expectedSpotValue, store.get("USDRUB", tickTime.plusHours(2)), 0.0);
     }
 }
